6109c08f5aa51e4e36fe1a1ea64d1cc5611976cd
Said <said.aguilar619@gmail.com>
2020-03-10 11:40:28 -0700

    fixed some stuff

--- Develop/app.js
+++ Develop/app.js
@@ -1,17 +1,15 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
 
-
-const allEmployees = [];
+const employees = [];
 
 const initTeamQuestions = [
     { type: "input", name: "name", message: "What is the manager's name?" },
@@ -39,26 +37,123 @@ const initTeamQuestions = [
     }
 ];
 
+const newEngineerQuestions = [
+    { type: "input", name: "name", message: "What is the engineer's name?" },
+    { type: "input", name: "id", message: "What is the engineer's ID?" },
+    { type: "input", name: "email", message: "What is the engineer's email?" },
+    {
+        type: "input",
+        name: "github",
+        message: "What is their GitHub username?"
+    },
+    {
+        type: "list",
+        name: "addTeamMember",
+        message: "Would you like to add another team member?",
+        choices: [
+            {
+                name: "Add an engineer",
+                value: "engineer"
+            },
+            {
+                name: "Add an intern",
+                value: "intern"
+            },
+            {
+                name: "No additional team members",
+                value: "no"
+            }
+        ]
+    }
+];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+const newInternQuestions = [
+    { type: "input", name: "name", message: "What is the intern's name?" },
+    { type: "input", name: "id", message: "What is the intern's ID?" },
+    { type: "input", name: "email", message: "What is the intern's email?" },
+    { type: "input", name: "school", message: "What school do they attend?" },
+    {
+        type: "list",
+        name: "addTeamMember",
+        message: "Would you like to add another team member?",
+        choices: [
+            {
+                name: "Yes, let's add an engineer",
+                value: "engineer"
+            },
+            {
+                name: "Yes, let's add an intern",
+                value: "intern"
+            },
+            {
+                name: "No, there are no additional team members",
+                value: "no"
+            }
+        ]
+    }
+];
+
+const initTeam = () => {
+    inquirer.prompt(initTeamQuestions).then(function(data) {
+        const manager = new Manager(
+            data.name,
+            data.id,
+            data.email,
+            data.officeNumber
+        );
+
+        employees.push(manager);
+        whatsNext(data);
+    });
+};
+
+const addNewEngineer = () => {
+    inquirer.prompt(newEngineerQuestions).then(function(data) {
+        const engineer = new Engineer(
+            data.name,
+            data.id,
+            data.email,
+            data.github
+        );
+
+        employees.push(engineer);
+        whatsNext(data);
+    });
+};
+
+const addNewIntern = () => {
+    inquirer.prompt(newInternQuestions).then(function(data) {
+        const intern = new Intern(
+            data.name,
+            data.id,
+            data.email,
+            data.school
+        );
+
+        employees.push(intern);
+        whatsNext(data);
+    });
+};
+
+const whatsNext = (data) => {
+    if (data.addTeamMember === "engineer") {
+        addNewEngineer();
+    } else if (data.addTeamMember === "intern") {
+        addNewIntern();
+    } else {
+        console.log("Building your HTML");
+        generateHTML();
+    }
+}
+
+const generateHTML = () => {
+    const htmlString = render(employees)
+    fs.writeFile(outputPath, htmlString, function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Success!");
+    });
+}
+
+initTeam();
